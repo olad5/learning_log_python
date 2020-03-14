d0c04ebc1c3f591d9e8c3f8d4d82acebbb1d2b4b
shonubifrancis <shonubioladipo@gmail.com>
2020-03-14 01:32:52 +0100

    initial commit

---
+++ ll-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/oladipo/python_work/python_crash_course/learning_log/ll-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
