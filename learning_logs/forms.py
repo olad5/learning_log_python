76ae5a8b68b6e618af451f7964d191e7f952bda7
shonubifrancis <shonubioladipo@gmail.com>
2020-04-01 21:10:56 +0100

    completed 20.1

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -14,6 +14,6 @@ class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ["text"]
-        labels = {"text": "Entry:"}
-        widgets = {"text": forms.Textarea(attrs={"cols": 80})}
-
+        labels = {"text": ""}
+        widgets = {"text": forms.Textarea(attrs={"placeholder": "Entry:"})}
+        # widgets = {"text": forms.Textarea(attrs={"cols": 80})}
